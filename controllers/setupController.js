c470ff3cc71cb4209b11f4e03a96c432df1cb0cf
ratneshchipre <ratneshchipre@gmail.com>
2025-06-23 18:28:24 +0530

    feat: Add update company data functionality and corresponding route

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -85,7 +85,72 @@ const getCompanyData = async (req, res) => {
   }
 };
 
+const updateCompanyData = async (req, res) => {
+  const { userId } = req.params;
+  const { name, address, industry, role, size } = req.body;
+
+  if (!userId) {
+    return res.status(400).json({
+      success: false,
+      message: "No user found.",
+    });
+  }
+
+  if (!name || !address || !industry || !role || !size) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required.",
+    });
+  }
+
+  try {
+    const company = await Company.findOneAndUpdate(
+      {
+        userId,
+      },
+      {
+        name,
+        address,
+        industry,
+        role,
+        size,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!company) {
+      return res.status(404).json({
+        success: false,
+        message: "Failed to update company data.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Company data updated successfully.",
+      companyData: {
+        dataId: company._id,
+        name: company.name,
+        address: company.address,
+        industry: company.industry,
+        role: company.role,
+        size: company.size,
+      },
+    });
+  } catch (error) {
+    console.error("Error updating company data:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while updating company data.",
+    });
+  }
+};
+
 module.exports = {
   handleAccountSetup,
   getCompanyData,
+  updateCompanyData,
 };
--- routes/setupRoutes.js
+++ routes/setupRoutes.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const {
   handleAccountSetup,
   getCompanyData,
+  updateCompanyData,
 } = require("../controllers/setupController");
 const auth = require("../middlewares/authMiddleware");
 
 router.post("/", auth, handleAccountSetup);
-router.get("/:userId", auth, getCompanyData);
+router
+  .get("/:userId", auth, getCompanyData)
+  .patch("/:userId", auth, updateCompanyData);
 
 module.exports = router;
